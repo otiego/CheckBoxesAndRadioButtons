c0f14d78b629b460641a152f2d8f44a0bd948766
Ronald <ronald.otiego@gmail.com>
2025-11-06 20:08:23 +0300

    click order

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,9 @@ plugins {
 }
 
 android {
+    buildFeatures {
+        viewBinding = true
+    }
     namespace = "com.example.checkboxesandradiobuttons"
     compileSdk {
         version = release(36)
