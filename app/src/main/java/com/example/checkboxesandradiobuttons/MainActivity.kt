af4815db6891889565751e1b4ac4895d4a230700
Ronald <ronald.otiego@gmail.com>
2025-11-06 20:08:23 +0300

    click order button

--- app/src/main/java/com/example/checkboxesandradiobuttons/MainActivity.kt
+++ app/src/main/java/com/example/checkboxesandradiobuttons/MainActivity.kt
@@ -1,20 +1,45 @@
 package com.example.checkboxesandradiobuttons
 
 import android.os.Bundle
+import android.widget.RadioButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.checkboxesandradiobuttons.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.btnOrder.setOnClickListener {
+            val checkedMeatRadioButtonId = binding.rgMeat.checkedRadioButtonId
+            val meat = findViewById<RadioButton>(checkedMeatRadioButtonId)
+            val cheese = binding.cbCheese.isChecked
+            val onions = binding.cbOnions.isChecked
+            val salad = binding.cbSalad.isChecked
+            val orderString = "You ordered a burger with:\n" +
+                    "${meat.text}" +
+                    (if (cheese) "\nCheese" else "") +
+                    (if (onions) "\nOnions" else "") +
+                    (if (salad) "\nSalad" else "")
+            binding.tvOrder.text = orderString
+
         }
     }
 }
